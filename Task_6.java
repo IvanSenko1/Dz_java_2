e863dd32831266d46b14dff779e24ea12d1e8ea6
Ivan <senko.ivan999@mail.ru>
2023-01-22 10:58:25 +0300

    Задача_6

---
+++ Task_6.java
@@ -0,0 +1,12 @@
+public class Task_6 {
+	public static void main(String[] args) {
+		String initialString = "'='";
+		StringBuilder modifiedString = new StringBuilder(initialString);
+
+		int index = initialString.indexOf("=");
+		modifiedString.replace(index, index + 1, "равно");
+
+		System.out.println("\nНачальная строка: \033[31m" + initialString + "\033[0m");
+		System.out.println("Новая строка: \033[31m" + modifiedString + "\033[0m");
+	}
+}
